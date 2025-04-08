0b58f8f270ce7b7fcd238626b8afb2d044e27e8d
fawad1997 <fawad_12@outlook.com>
2025-04-08 10:49:20 -0700

    updated shops page UI

--- types/shop.ts
+++ types/shop.ts
@@ -7,11 +7,16 @@ export interface Shop {
   zip_code: string;
   phone_number: string;
   email: string;
+  opening_time: string;
+  closing_time: string;
   average_wait_time: number;
   has_advertisement: boolean;
   advertisement_image_url?: string;
   advertisement_start_date?: string;
   advertisement_end_date?: string;
   is_advertisement_active: boolean;
+  estimated_wait_time: number;
+  is_open: boolean;
+  formatted_hours: string;
   owner_id: number;
 }
